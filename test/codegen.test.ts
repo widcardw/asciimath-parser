aefa8e633ea39576f18393b3d97483ba289d1f51
widcardw <lui747353367@qq.com>
2023-01-28 12:16:00 +0800

    fix: add aligned env

--- test/codegen.test.ts
+++ test/codegen.test.ts
@@ -20,4 +20,15 @@ describe('codegen matrix', () => {
   it('should generate cases', () => {
     expect(codegen(parser(trie.tryParsingAll('f(x) = { x^2, if x>0; x, otherwise :}')))).toMatchSnapshot()
   })
+
+  it('should generate recursive matrix', () => {
+    expect(codegen(parser(trie.tryParsingAll('[(0;0),(0;1);(1;0),(1;1)]')))).toMatchSnapshot()
+  })
+})
+
+describe('codegen align', () => {
+  const trie = createTrie()
+  it('should generate aligend environment', () => {
+    expect(codegen(parser(trie.tryParsingAll('varphi(x) & = Phi\'(x) \n\n & = 1/(sqrt(2pi) sigma) "e"^(- ((x-mu)^2)/(2sigma^2))')))).toMatchSnapshot()
+  })
 })
